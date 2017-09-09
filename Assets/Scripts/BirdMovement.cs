729a7013cd6ea06430d154e429a63abd74f26a18
Arshd <artourky@yahoo.com>
2017-09-09 02:07:57 +0200

    Camera Tracking

---
+++ Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class BirdMovement : MonoBehaviour {
+
+    public float jumpForce = 2f;
+    public float maxForce = 5f;
+    public float forwardSpeed = 1f;
+
+    Rigidbody2D myRigidbody;
+
+    bool didFlap = false;
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            didFlap = true;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        myRigidbody.velocity = new Vector2(forwardSpeed, myRigidbody.velocity.y);
+
+        float angle = 0f;
+
+        if (didFlap)
+        {
+            didFlap = false;
+
+            if (myRigidbody.velocity.y < 0)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
+            }
+
+            myRigidbody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            myRigidbody.velocity = Vector3.ClampMagnitude(myRigidbody.velocity, maxForce);
+        }
+
+        if (myRigidbody.velocity.y < 0)
+        {
+            angle = Mathf.Lerp(0, -90, -myRigidbody.velocity.y / maxForce);
+        }
+        else
+        {
+            angle = Mathf.Lerp(0, 90, myRigidbody.velocity.y / maxForce);
+        }
+
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+}
---
+++ Assets/Scripts/CameraOffset.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CameraOffset : MonoBehaviour {
+
+    public Transform player;
+
+    float offsetX;
+
+	// Use this for initialization
+	void Start () {
+        if (player)
+        {
+            offsetX = transform.position.x - player.position.x;
+            print(offsetX);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (player)
+        {
+            Vector3 pos = transform.position;
+            pos.x = player.position.x + offsetX;
+            transform.position = pos;
+        }
+    }
+}
